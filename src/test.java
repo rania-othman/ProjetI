a5e672ef9bc1576ced7ad2b097f254bc81234b41
Rania Othman <rania.othman.int@groupe-gema.com>
2024-11-14 02:34:42 +0100

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Donner une valeur");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("La valeur de x " + x);
+    }
+}
